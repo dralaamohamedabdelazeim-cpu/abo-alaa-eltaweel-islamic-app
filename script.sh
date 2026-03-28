8a3456375614a686f3da3c14e6c5c104fdb9cf23
dralaamohamedabdelazeim-cpu <dr.alaamohamedabdelazeim@gmail.com>
2026-03-28 10:31:04 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-# نسخة معالجة لمشكلة الويندوز
+# نسخة معدلة بدون مشاكل الويندوز
 APP_DIR="MohamedAbdelazimApp"
 mkdir -p $APP_DIR/app/src/main/java/com/abdelazim/islamicapp
 mkdir -p $APP_DIR/app/src/main/res/layout
 mkdir -p $APP_DIR/app/src/main/res/drawable
 mkdir -p $APP_DIR/app/src/main/res/values
 
-# إنشاء ملف النصوص
+# النصوص (اسم التطبيق)
 echo '<resources><string name="app_name">تطبيق محمد عبد العظيم الطويل</string></resources>' > $APP_DIR/app/src/main/res/values/strings.xml
 
-# إنشاء الواجهة وصورة الوالد
+# الواجهة (صورة الوالد في الخلفية)
 cat <<EOF > $APP_DIR/app/src/main/res/layout/activity_main.xml
 <RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
     android:layout_width="match_parent" android:layout_height="match_parent"
     android:background="@drawable/father_background">
     <TextView android:layout_width="wrap_content" android:layout_height="wrap_content"
         android:text="صدقة جارية: محمد عبد العظيم الطويل" android:layout_centerInParent="true" 
-        android:textColor="#FFFFFF" android:textSize="20sp"/>
+        android:textColor="#FFFFFF" android:textSize="20sp" android:textStyle="bold"/>
 </RelativeLayout>
 EOF
 
-# كود البرمجة
+# الكود البرمجي
 cat <<EOF > $APP_DIR/app/src/main/java/com/abdelazim/islamicapp/MainActivity.java
 package com.abdelazim.islamicapp;
 import android.os.Bundle;
